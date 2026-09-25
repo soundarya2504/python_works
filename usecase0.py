1613d3398fefa9fa67f8b13996df4a01b1f4f2e5
soundarya2504 <soundarya8838@gmail.com>
2026-09-25 11:55:52 +0530

    Update usecase0.py

--- usecase0.py
+++ usecase0.py
@@ -1,3 +1,15 @@
+Conditional Structure
+Use Case 0: Banking Eligibility Check
+Write a program that asks the user for:
+Age
+Monthly income
+
+Conditions:
+If age < 18: print "Not eligible for a bank account."
+If age >= 18 and income < 15000: print "Eligible for basic savings account."
+If age >= 18 and income between 15000 and 50000: print "Eligible for savings + salary account."
+If age >= 18 and income > 50000: print "Eligible for premium account."
+
 Age=int(input("Age"))
 Monthly_income=int(input("Monthly income:"))
 if Age<18:
